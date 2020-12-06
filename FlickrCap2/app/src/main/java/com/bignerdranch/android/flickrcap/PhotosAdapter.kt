4ba9a1977a102c46868bea10b70a8470809de91f
Abdullahassem <71944810+Abdullahassem@users.noreply.github.com>
2020-12-06 14:23:48 +0300

    added the needed photos adapter

---
+++ FlickrCap2/app/src/main/java/com/bignerdranch/android/flickrcap/PhotosAdapter.kt
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.theflickrcapstone
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.theflickrcapstone.R
+import com.bignerdranch.android.theflickrcapstone.Photo
+
+class PhotosAdapter(var photos:List<Photo>):RecyclerView.Adapter<PhotoHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder=
+        PhotoHolder(LayoutInflater.from(parent.context).inflate(R.layout.photo_holder,parent,false))
+
+    override fun onBindViewHolder(holder: PhotoHolder, position: Int) { holder.bind(photos[position]) }
+
+    override fun getItemCount(): Int =photos.size
+
+}
